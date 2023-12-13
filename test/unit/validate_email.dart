94dc70f187078d525605e4a5a7172ce21522a7d8
naim114 <naim.muhammad99@gmail.com>
2023-12-13 15:27:18 +0800

    Add unit test case

---
+++ test/unit/validate_email.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:triviazilla/src/services/helpers.dart';
+
+void main() {
+  group('validateEmail', () {
+    test('should return true for a valid email', () {
+      debugPrint("should return true for a valid email");
+      debugPrint("sample: test@example.com");
+
+      // Arrange
+      final emailController = TextEditingController(text: 'test@example.com');
+
+      // Act
+      final result = validateEmail(emailController);
+
+      debugPrint("Result: $result");
+
+      // Assert
+      expect(result, true);
+    });
+
+    test('should return false for an empty email', () {
+      debugPrint("should return false for a valid email");
+      debugPrint("sample: ");
+
+      // Arrange
+      final emailController = TextEditingController(text: '');
+
+      // Act
+      final result = validateEmail(emailController);
+
+      debugPrint("Result: $result");
+
+      // Assert
+      expect(result, false);
+    });
+
+    test('should return false for an invalid email', () {
+      debugPrint("should return false for a valid email");
+      debugPrint("sample: invalid_email");
+
+      // Arrange
+      final emailController = TextEditingController(text: 'invalid_email');
+
+      // Act
+      final result = validateEmail(emailController);
+
+      debugPrint("Result: $result");
+
+      // Assert
+      expect(result, false);
+    });
+  });
+}
